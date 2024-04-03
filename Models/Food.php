811650fdaf55dd15953c1474f09ed9de9afe530d
s.ditaranto <simodita99@gmail.com>
2024-04-03 14:16:47 +0200

    first commit

---
+++ Models/Food.php
@@ -0,0 +1,11 @@
+<?php
+
+class Food extends Product {
+
+    function __construct($_name, $_price, $_category) {
+
+        parent::__construct($_name, $_price, $_category);
+        
+    }
+
+}
---
+++ Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+class Product {
+
+    protected $name;
+    protected $price;
+    protected $category;
+    public $thumb;
+    
+    /**
+     * __construct
+     *
+     * @param  string $_name
+     * @param  string $_price
+     * @param  Category $_category
+     */
+    function __construct($_name, $_price, $_category) {
+        $this->name = $_name;
+        $this->price = $_price;
+        $this->category = $_category;
+    }
+
+
+}
